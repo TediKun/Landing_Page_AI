689b1212465ff61910c042f46824706af8ec966d
TediKun <whyiqbal6@gmail.com>
2024-07-09 21:17:44 +0700

    Renponsive all devices, design background, update ui

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,22 @@
 /**
-*** TediKun
-*** tedisenpai7@gmail.com
+ *** TediKun
+ *** tedisenpai7@gmail.com
  */
+
+// Back Home
+window.addEventListener("scroll", function () {
+    const backHome = document.querySelector(".scroll_top");
+    backHome.classList.toggle("active", window.scrollY > 500);
+});
+
+// Nav Toggle
+const menuNav = document.querySelector(".nav__menu");
+const BtnNav = document.querySelector(".btn__menu");
+const icnBtnNav = BtnNav.querySelector("span");
+
+BtnNav.addEventListener("click", () => {
+    menuNav.classList.toggle("active");
+
+    const isActive = menuNav.classList.contains("active");
+    icnBtnNav.setAttribute("class", isActive ? "fa fa-close" : "fa fa-bars");
+});
